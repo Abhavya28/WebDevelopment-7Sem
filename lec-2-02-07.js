1062f69cc045ae1f0597abadba6fdc1a1f4f44e2
Abhavya28 <abhavya2801@gmail.com>
2025-07-03 11:17:37 +0530

    Intial Commit

---
+++ lec-2-02-07.js
@@ -0,0 +1,38 @@
+// //Main Higher Order Fcn
+// function calculate(logicFunction,a,b){
+//     return logicFunction(a,b);
+// }
+// //Logic Function
+// function add(a,b){
+//     return a+b;
+// }
+// //Logic Function
+// function sub(a,b){
+//     return a-b;
+// }
+// console.log(add,3,5);
+// console.log(sub,8,5);
+
+// //Higher Order Student Function
+// function studentDetails(name,fees){
+//     const emiAmount = emi(fees);
+//     return welcome(name,fees,emiAmount);
+// }
+// function emi(fees){
+//     return fees/10;
+// }
+// function welcome(name,fees,emiAmount){
+//     return `${name} with fees of ${fees} and emi of ${emiAmount}`;
+// }
+
+// console.log(studentDetails("Abhavya",30000));
+
+// //SETTIMEOUT
+// setTimeout(() => {
+//     console.log("Hello SetTimeout");
+// },3000);
+
+// //SETINTERVAL
+// setInterval(() => {
+//     console.log("Set Interval");
+// },2000);
