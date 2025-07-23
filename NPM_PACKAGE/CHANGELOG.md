7cc7d25d72d507b8cbae420d3b991070ff3d26c7
Abhavya28 <abhavya2801@gmail.com>
2025-07-23 11:50:49 +0530

    NPM_PACKAGE added

---
+++ NPM_PACKAGE/CHANGELOG.md
@@ -0,0 +1,7 @@
+# g-5-npm-package-1
+
+## 1.1.0
+
+### Minor Changes
+
+- Mul fcn added
---
+++ NPM_PACKAGE/index.js
@@ -0,0 +1,19 @@
+const addTwoNumbers = () =>{
+    console.log("Sum of two numbers");
+}
+
+
+const subNumbers = () =>{
+    console.log("Sub of two numbers");
+}
+
+const divNumbers = () =>{
+    console.log("division");
+}
+
+const mulNumbers = () =>{
+    console.log("multiplication");  
+}
+
+module.export = {addTwoNumbers, subNumbers, divNumbers, mulNumbers};
+
